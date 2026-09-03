bcfb3629b0f414974fd8e951f0f9937389269818
samialehegn1-a11y <samialehegn1@gmail.com>
2026-09-03 22:39:46 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,5 +20,17 @@ class MainActivity : ComponentActivity() {
                 MainScaffold(viewModel = viewModel)
             }
         }
+    }
+
+    // የመረጡትን ሰርቨር ሴቭ ማድረጊያ
+    fun saveSelectedServer(context: Context, serverId: String) {
+        val prefs = context.getSharedPreferences("AlphaVPN_Prefs", Context.MODE_PRIVATE)
+        prefs.edit().putString("SELECTED_SERVER_ID", serverId).apply()
+    }
+
+    // አፑ ሲከፈት የተመረጠውን ሰርቨር ማነቢያ
+    fun getSavedServer(context: Context): String {
+        val prefs = context.getSharedPreferences("AlphaVPN_Prefs", Context.MODE_PRIVATE)
+        return prefs.getString("SELECTED_SERVER_ID", "AUTO_SELECT") ?: "AUTO_SELECT"
     }
 }
